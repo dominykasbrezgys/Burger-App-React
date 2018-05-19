d7174b573712b1fbe603be868692dbcad598203b
dominykasbrezgys <d.brezgys@gmail.com>
2018-05-19 14:56:05 +0100

    Added Eat me button TODO: button styling

--- src/components/MenuGrid/MenuGrid.js
+++ src/components/MenuGrid/MenuGrid.js
@@ -2,17 +2,31 @@ import React from 'react';
 import MenuItem from './MenuItem/MenuItem';
 import AddBurgerItem from './MenuItem/AddBurgerItem/AddBurgerItem';
 import classes from './MenuGrid.css';
-import Burger from '../Burger/Burger'
+import Burger from '../Burger/Burger';
+import Button from '../UI/Button/Button';
 
 const MenuGrid = (props) =>{
-    console.log(Object.values(props.burgers));
-    const burgers = Object.values(props.burgers).map((burger,index) =>{
+
+    const burgers = Object.keys(props.burgers).map(bKey =>{
         return (
-            <MenuItem key={index}>
-                <h1>{burger.burgerName}</h1>
-                <Burger ingredients={burger.ingredients} forGrid/> 
+            <MenuItem key={bKey}>
+                <h1>{props.burgers[bKey].burgerName}</h1>
+                <Burger ingredients={props.burgers[bKey].ingredients} forGrid/>
+                <Button buttonType ='EatMe' clicked={()=>props.eatMe(bKey)}>
+                    Eat me
+                </Button>
             </MenuItem>)
     });
+
+    // const burgers = Object.values(props.burgers).map((burger,index) =>{
+
+    //     return (
+    //         <MenuItem 
+    //             key={index}>
+    //             <h1>{burger.burgerName}</h1>
+    //             <Burger ingredients={burger.ingredients} forGrid/> 
+    //         </MenuItem>)
+    // });
 
     return (
         <section className={classes.MenuGrid}>
--- src/containers/Menu/Menu.js
+++ src/containers/Menu/Menu.js
@@ -9,6 +9,25 @@ class Menu extends Component {
         loading: true
     }
 
+    eatMeHandler = (burgerID) =>{
+        axios.get('/burgers.json')
+            .then(res =>{
+                const burger = res.data[burgerID];
+                let alertMsg = 'Burger not found. Sorry.';
+                if(burger){
+                    alertMsg = (
+                        'Unique ID stored in Firebase: '+burgerID+'\n'+
+                        'Burger Name: '+burger.burgerName+'\n'+
+                        'Ingredients: '+'\n'+
+                        Object.keys(burger.ingredients).map(ingredientKey =>{
+                            return ingredientKey+': '+burger.ingredients[ingredientKey]+'\n';
+                        }));
+                }
+                alert(alertMsg);
+            })
+            .catch(error => alert(error));
+    }
+
     componentDidMount(){
         axios.get('/burgers.json')
         .then(res =>{
@@ -18,7 +37,8 @@ class Menu extends Component {
     }
 
     render(){
-        return this.state.loading ? <Spinner/> : <MenuGrid burgers = {this.state.burgers}/>
+        return this.state.loading ? <Spinner/> : 
+            <MenuGrid burgers = {this.state.burgers} eatMe ={this.eatMeHandler}/>
     }
 }
 
