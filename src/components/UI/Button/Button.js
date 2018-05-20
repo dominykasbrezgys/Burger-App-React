860cd9ef43d0ceee8e90ec63a92e16a722ab0cc6
dominykasbrezgys <d.brezgys@gmail.com>
2018-05-20 12:12:07 +0100

    Revert "Minor changes to Button styling"
    This reverts commit 3982e5ca4bbb9940ffff5e78306b0a97c3093b95.

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,15 +1,20 @@
 import React from  'react';
 import classes from './Button.css';
 
-const button = (props) => (
+const button = (props) => {
+    const styles = [classes.Button, classes[props.buttonType]] ;
+    if (props.isDisabled){
+        styles.push(classes.Disabled);
+    }
+    return(
         <button
-            className ={[classes.Button, classes[props.buttonType]].join(' ')}
+            className ={styles.join(' ')}
             onClick={props.clicked}
             disabled={props.isDisabled}>
             {props.children}
         </button>
-);
-
+    );
+}
 
 
 export default button;
